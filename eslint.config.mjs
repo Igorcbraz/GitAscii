1f24848e7e55c9a3a0fc8d1b8a5ea65d740199a1
Igor Costa Braz <Igorcbraz1@gmail.com>
2026-08-04 19:24:53 -0300

    build: setup testing and storybook infrastructure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { FlatCompat } from '@eslint/eslintrc'
 import prettierConfig from 'eslint-config-prettier'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
@@ -14,36 +17,31 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-export default [
-  { ignores: ['.next/**', 'node_modules/**'] },
-  ...fixupConfigRules(
-    compat.config({
-      extends: ['next/core-web-vitals'],
-    })
-  ),
-  prettierConfig,
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-      'unused-imports': unusedImports,
-    },
-    rules: {
-      'no-console': 'off',
-      'no-debugger': 'error',
-      'prefer-const': 'error',
-      eqeqeq: ['error', 'always'],
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-    },
+export default [{ ignores: ['.next/**', 'node_modules/**'] }, ...fixupConfigRules(
+  compat.config({
+    extends: ['next/core-web-vitals'],
+  })
+), prettierConfig, {
+  plugins: {
+    'simple-import-sort': simpleImportSort,
+    'unused-imports': unusedImports,
   },
-]
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    eqeqeq: ['error', 'always'],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+}, ...storybook.configs["flat/recommended"]];
